548639bf26a4a3dc9a779f87257142ebbed1a746
Craig Newcomb <craignewcomb@craigs-imac.local>
2018-09-12 16:59:29 -0500

    finished layout for iPhone devices

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -9,15 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
     override func viewDidLoad() {
+        profilePhoto.layer.cornerRadius = 8.0
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
